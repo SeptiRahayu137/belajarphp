0de760892e6c2f17bcc06e24a19e6d9456d75216
SeptiRahayu137 <rhysepti900@gmail.com>
2024-12-13 20:25:30 +0700

    belajardatabase

---
+++ kontak.php
@@ -0,0 +1,18 @@
+<h2>kontak</h2>
+<form action="Simpan.php" method="post">
+    <label>Nama</label>
+    <input type="text" name="nama" >
+    <br>
+
+    <label>email</label>
+    <input type="email" name="email" >
+    <br>
+
+    <label>Nomor HP</label>
+    <input type="tel" name="telp" >
+    <br>
+
+    <button type="submit">Simpan</button>
+</form>
+
+    
